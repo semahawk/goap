ae0d51d3999609deaf725f9c8db2773ea4edcca9
Szymon Urbaś <szymon.urbas@aol.com>
2017-04-19 21:43:42 +0200

    Save the current state - working on pathfinding

--- examples/simple.rs
+++ examples/simple.rs
@@ -13,9 +13,11 @@ enum Action {
   Attack,
   Flee,
   Explode,
+  FindWeapon,
+  Approach,
 }
 
-#[derive(Debug, Hash, Eq, PartialEq)]
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Ord, PartialOrd)]
 enum Condition {
   HasWeapon(bool),
   NearEnemy(bool),
@@ -26,6 +28,13 @@ fn main() {
   let mut ap = goap::ActionPlanner::new();
 
   ap.add_action(
+    Action::Explode,
+    vec!(Condition::NearEnemy(true)),
+    vec!(Condition::EnemyAlive(false)),
+    100
+  );
+
+  ap.add_action(
     Action::Attack,
     vec!(Condition::HasWeapon(true), Condition::NearEnemy(true)),
     vec!(Condition::EnemyAlive(false)),
@@ -34,24 +43,34 @@ fn main() {
 
   ap.add_action(
     Action::Flee,
-    vec!(Condition::HasWeapon(false)),
+    vec!(Condition::HasWeapon(false), Condition::NearEnemy(true)),
     vec!(Condition::NearEnemy(false)),
     10
   );
 
   ap.add_action(
-    Action::Explode,
+    Action::FindWeapon,
+    vec!(Condition::HasWeapon(false), Condition::NearEnemy(false)),
+    vec!(Condition::HasWeapon(true)),
+    10
+  );
+
+  ap.add_action(
+    Action::Approach,
+    vec!(Condition::HasWeapon(true)),
     vec!(Condition::NearEnemy(true)),
-    vec!(Condition::EnemyAlive(false)),
-    100
+    10
   );
 
   ap.set_state(Condition::NearEnemy(true));
   ap.set_state(Condition::HasWeapon(true));
+  ap.set_state(Condition::EnemyAlive(true));
 
   ap.set_goal(Condition::EnemyAlive(false));
 
   ap.display_actions();
+
+  println!("next step: {:?}", ap.plan_next_step());
 }
 
 /*
--- src/lib.rs
+++ src/lib.rs
@@ -6,33 +6,44 @@
 // Created on: 17 Apr 2017 12:45:39 +0200 (CEST)
 //
 
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, BinaryHeap, BTreeSet};
 
 use std::hash::Hash;
-use std::cmp::{Eq, PartialEq};
+use std::cmp::{Ordering, Eq, PartialEq};
 
 #[derive(Debug)]
 pub struct ActionPlanner<A, C>
 where A: Hash + Eq + PartialEq,
-      C: Hash + Eq + PartialEq {
-  actions: HashMap<A, (Vec<C>, Vec<C>, usize)>,
-  states: HashSet<C>,
-  goals: HashSet<C>,
+      C: Hash + Eq + PartialEq + Clone {
+  actions: HashMap<A, (BTreeSet<C>, BTreeSet<C>, usize)>,
+  states: BTreeSet<C>,
+  goal: BTreeSet<C>,
 }
 
 impl<A, C> ActionPlanner<A, C>
 where A: Hash + Eq + PartialEq,
-      C: Hash + Eq + PartialEq {
+      C: Hash + Eq + PartialEq + Clone + Ord {
   pub fn new() -> ActionPlanner<A, C> {
     ActionPlanner {
       actions: HashMap::new(),
-      states: HashSet::new(),
-      goals: HashSet::new(),
+      states: BTreeSet::new(),
+      goal: BTreeSet::new(),
     }
   }
 
   pub fn add_action(&mut self, action: A, preconditions: Vec<C>, effects: Vec<C>, cost: usize) {
-    self.actions.insert(action, (preconditions, effects, cost));
+    let mut new_preconditions = BTreeSet::new();
+    let mut new_effects = BTreeSet::new();
+
+    for precondition in preconditions {
+      new_preconditions.insert(precondition);
+    }
+
+    for effect in effects {
+      new_effects.insert(effect);
+    }
+
+    self.actions.insert(action, (new_preconditions, new_effects, cost));
   }
 
   pub fn set_state(&mut self, state: C) {
@@ -40,15 +51,93 @@ where A: Hash + Eq + PartialEq,
   }
 
   pub fn set_goal(&mut self, goal: C) {
-    self.goals.insert(goal);
+    self.goal.insert(goal);
+  }
+
+
+  pub fn plan_next_step(&mut self) -> Vec<&A>
+  where A: std::fmt::Debug, C: std::fmt::Debug {
+    println!("===");
+    println!("=== finding a way to {:?}", self.goal);
+    println!("===");
+    let mut plan: Vec<&A> = Vec::new();
+
+    fn approx_distance_to<C>(from: &BTreeSet<C>, goal: &BTreeSet<C>) -> usize
+    where C: std::cmp::Ord + std::fmt::Debug {
+      goal.difference(from).count()
+    }
+
+    #[derive(Debug, Clone, Eq, PartialEq)]
+    struct Node<T>
+    where T: Hash + Eq + PartialEq {
+      states: BTreeSet<T>,
+      cost: usize,
+    }
+
+    impl<T> Ord for Node<T>
+    where T: Hash + Eq {
+      fn cmp(&self, other: &Node<T>) -> Ordering {
+        other.cost.cmp(&self.cost)
+      }
+    }
+
+    impl<T> PartialOrd for Node<T>
+    where T: Hash + Eq + PartialEq {
+      fn partial_cmp(&self, other: &Node<T>) -> Option<Ordering> {
+        Some(self.cmp(other))
+      }
+    }
+
+    let mut came_from: HashMap<BTreeSet<C>, Option<Node<C>>> = HashMap::new();
+    let mut cost_so_far: HashMap<BTreeSet<C>, usize> = HashMap::new();
+
+    let mut frontier = BinaryHeap::new();
+
+    came_from.insert(self.states.clone(), None);
+    cost_so_far.insert(self.states.clone(), 0);
+    frontier.push(Node { states: self.states.clone(), cost: 0 });
+
+    'find_plan: while let Some(current_node) = frontier.pop() {
+      println!("-- popped node from the frontier: {:?}", current_node);
+
+      'find_action: for (action, &(ref preconds, ref effects, cost)) in &self.actions {
+        println!("---- checking action {:?} ({:?} -> {:?})", action, preconds, effects);
+        for precond in preconds {
+          if let None = self.states.get(precond) {
+            continue 'find_action;
+          }
+        }
+
+        println!("------ fulfills all preconditions!");
+
+        //println!("current_node.states vs the goal: {:?} vs {:?}", current_node.states, self.goal);
+
+        let new_cost = cost_so_far.get(&current_node.states).unwrap() + cost;
+        if !cost_so_far.contains_key(&preconds) || new_cost < *cost_so_far.get(&preconds).unwrap() {
+          //println!("cost_so_far: {:?}", cost_so_far);
+          //println!("quote next: {:?}", preconds);
+          let mut new_state = self.states.clone();
+          for effect in effects {
+            new_state.insert(effect.clone());
+          }
+          cost_so_far.insert(preconds.clone(), new_cost);
+          println!("------ pushing {:?} onto the frontier", action);
+          frontier.push(Node { states: new_state, cost: new_cost + approx_distance_to(effects, &self.goal) });
+        }
+
+        plan.push(action);
+      }
+    }
+
+    plan
   }
 
   pub fn display_actions(&self)
   where A: std::fmt::Debug, C: std::fmt::Debug {
-    for (ref action, ref cond) in &self.actions {
+    for (ref action, &(ref preconds, ref effects, ..)) in &self.actions {
       println!("action: {:?}", action);
-      println!("-- preconditions: {:?}", cond.0);
-      println!("-- effects: {:?}", cond.1);
+      println!("-- preconditions: {:?}", preconds);
+      println!("-- effects: {:?}", effects);
     }
   }
 }
